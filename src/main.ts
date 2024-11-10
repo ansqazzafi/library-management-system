78c37c32a4d2f7aa0c9b50a120b24a8a04ea32e4
ansqazzafi <ans.qazzafi@bracketsltd.email>
2024-11-09 22:40:40 -0800

    crud

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-
+import { GlobalExceptionFilter } from './GlobalExceptionFilter/global-exception-filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -13,6 +13,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
       transform: true,
     }),)
+    app.useGlobalFilters(new GlobalExceptionFilter())
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
